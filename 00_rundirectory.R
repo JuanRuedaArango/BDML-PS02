a001ed53df1c5ee541b802218a280add534498cc
hernanchoporras-afk <hernanchoporras@gmail.com>
2026-03-24 16:34:12 -0500

    Create 00_rundirectory.R

---
+++ 00_rundirectory.R
@@ -0,0 +1,14 @@
+##########################################################
+# Master script
+#
+# Running this file reproduces all results in the repository.
+#
+# To reproduce all results, run:
+# from an interactive R session: source("01_code/00_rundirectory.R")   
+# or from the command line: R CMD BATCH 01_code/00_rundirectory.R
+#
+# Authors:
+# - Hernán Yepes
+# - Juan Rueda
+# - Andrés Suárez
+##########################################################
